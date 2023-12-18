274f127da4f9aae7853a1578f74d1eec6ceaac7a
Pranie63 <Pranav Ramkumar>
2023-12-18 12:21:41 -0500

    Added Content tsx and css files

---
+++ src/Content.tsx
@@ -0,0 +1,15 @@
+import './Content.css'
+
+type ContentProps = {
+    projectMappings: Map<String, Array<String>>
+}
+
+function Content(props: ContentProps) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Content
--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Portfolio.css';
 import Intro from './Intro';
+import Content from './Content'
 
 function Portfolio() {
     return(
         <div className='body'>
             <Intro name="Pranav" titles={["Software Engineer", "Software Developer"]} description={"Test"}/>
+            <Content projectMappings={new Map<String, Array<String>>()}/>
         </div>
     )
 }
