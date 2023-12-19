57343ff8e4f669f0c0dbd74d72c6c1b2589a4c2d
Pranie63 <Pranav Ramkumar>
2023-12-19 17:13:26 -0500

    Created project mappings to interface with site

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Portfolio.css';
 import Intro from './Intro';
-import Content from './Content'
+import Content from './Content';
+
+const projectMappings: Map<String, Array<String>> = new Map<String, Array<String>>([["test", ["test"]], ["test1", ["test"]]]);
 
 function Portfolio() {
     return(
         <div className='body'>
             <Intro name="Pranav" titles={["Software Engineer", "Software Developer"]} description={"Test"}/>
-            <Content projectMappings={new Map<String, Array<String>>()}/>
+            <Content projectMappings={projectMappings}/>
         </div>
     )
 }
