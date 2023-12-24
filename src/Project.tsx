dc1703c03474473ca44a0852a3a30b0af05d0644
Pranie63 <Pranav Ramkumar>
2023-12-24 15:40:14 -0500

    Updated project display to make selection more apparent

--- src/Project.tsx
+++ src/Project.tsx
@@ -6,7 +6,7 @@ type ProjectProps = {
 
 function Project(props: ProjectProps) {
     return (
-        <div className='project'>
+        <div className='project' style={{backgroundImage: "url(https://t3.ftcdn.net/jpg/02/10/52/74/360_F_210527423_8Qdmmj7GlPEy0DknhPXtjigA54voMEDK.jpg)"}}>
             {props.name}
         </div>
     )
