f1a205e59274af57b92d22461e560ad016421504
Pranie63 <Pranav Ramkumar>
2024-01-02 14:29:34 -0500

    Created basic intro layout

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -8,11 +8,13 @@ type IntroProps = {
 
 function Intro(props: IntroProps) {
     return(
-        <div>
-            {props.name}
-            {props.titles[0]}
-            {props.titles[1]}
-            {props.description}
+        <div className='intro'>
+            <div className='img' style={{backgroundImage: "url(https://t3.ftcdn.net/jpg/02/10/52/74/360_F_210527423_8Qdmmj7GlPEy0DknhPXtjigA54voMEDK.jpg)"}}></div>
+            <div className='desc'>
+                <div className='section'>Hey, you have entered {props.name}'s domain!</div>
+                <div className='section'>{props.titles[0]} and {props.titles[1]}</div>
+                <div className='section'>{props.description}</div>
+            </div>
         </div>
     )
 }
--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -4,11 +4,12 @@ import Intro from './Intro';
 import Content from './Content';
 
 const projectMappings: Map<String, Array<String>> = new Map<String, Array<String>>([["Java", ["test", "1test"]], ["Javascript", ["test1", "1test1"]], ["Cybersecurity", ["1", "2", "3", "4", "5"]], ["Robotics", ["1", "2", "3", "4", "5"]]]);
+const description: String = "This is a portfolio containing a collection of my most memorable pieces of work. They extend from personal projects to cumulative course work, representing my journey as a software engineer."
 
 function Portfolio() {
     return(
         <div className='body'>
-            <Intro name="Pranav" titles={["Software Engineer", "Software Developer"]} description={"Test"}/>
+            <Intro name="Pranav Ramkumar" titles={["Software Engineer", "Software Developer"]} description={description}/>
             <Content projectMappings={projectMappings}/>
         </div>
     )
