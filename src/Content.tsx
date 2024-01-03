1931b5d2ab88bd7885ea03560b5acd08248d315c
Pranie63 <Pranav Ramkumar>
2024-01-02 19:38:36 -0500

    Updated content to change opacity on scroll

--- src/Content.tsx
+++ src/Content.tsx
@@ -14,12 +14,40 @@ function Content(props: ContentProps) {
             <div className='title'>
                 Projects
             </div>
-            <div className="content">
+            <div id = "content" className="content">
                 <ProjectCats projectMappings={props.projectMappings} setProjectCat={setProjectCat}/>
                 <Projects projects={props.projectMappings.get(curProjectCat)}/>
             </div>
         </div>
     )
 }
+
+// var header: HTMLElement | null = document.getElementById("content")
+
+// function fadeOutOnScroll(element: HTMLElement | null) {
+// 	if (!element) {
+// 		return;
+// 	}
+	
+// 	var distanceToTop = window.scrollY + element.getBoundingClientRect().top;
+// 	var elementHeight = element.clientHeight;
+// 	var scrollTop = document.documentElement.scrollTop;
+	
+// 	var opacity = 1;
+	
+// 	if (scrollTop > distanceToTop) {
+// 		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+// 	}
+	
+// 	if (opacity >= 0) {
+// 		element.style.opacity = opacity.toString();
+// 	}
+// }
+
+// function scrollHandler() {
+// 	fadeOutOnScroll(header);
+// }
+
+// window.addEventListener('scroll', scrollHandler);
 
 export default Content
