56e41642d824e4e63bb9300e6615d5bb2575952b
Pranie63 <Pranav Ramkumar>
2024-01-03 14:59:51 -0500

    Updated intro with professional styling and correct profile pic

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,4 +1,5 @@
 import './Intro.css'
+import profilePic from './ProfilePic.jpg'
 
 type IntroProps = {
     name: String
@@ -6,14 +7,23 @@ type IntroProps = {
     description: String
 }
 
+// const imgPath: String = "./../";
+
 function Intro(props: IntroProps) {
     return(
-        <div className='intro'>
-            <div className='img' style={{backgroundImage: "url(https://t3.ftcdn.net/jpg/02/10/52/74/360_F_210527423_8Qdmmj7GlPEy0DknhPXtjigA54voMEDK.jpg)"}}></div>
-            <div className='desc'>
-                <div className='section'>Hey, you have entered {props.name}'s domain!</div>
-                <div className='section'>{props.titles[0]} and {props.titles[1]}</div>
-                <div className='section'>{props.description}</div>
+        <div>
+            <div className='pagetitle'>
+                {props.name}'s Portfolio
+            </div>
+            <div className='intro'>
+                <div className='img'>
+                    <img src={profilePic}></img>
+                </div>
+                <div className='desc'>
+                    <div className='section'>Hey, you have entered {props.name}'s domain!</div>
+                    <div className='section'>{props.titles[0]} and {props.titles[1]}</div>
+                    <div className='section'>{props.description}</div>
+                </div>
             </div>
         </div>
     )
