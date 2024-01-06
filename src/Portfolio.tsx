30a88f0644dbfc4d2a2814def4cc5515dd79e941
Pranie63 <Pranav Ramkumar>
2024-01-06 17:34:47 -0500

    Updated projects to link to correct repositories

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -6,15 +6,18 @@ import type {ProjectProps} from './Project'
 import pic from './ProfilePic.jpg'
 import WordlePic from './WordlePic.png'
 import CourseReviewPic from './CourseReviewPic.png'
+import ListPic from './ListPic.png'
 
 // var test: Array<ProjectProps> = new Array<ProjectProps>({name: "test"}, {name: "test2"})
-const projectMappings: Map<String, Array<ProjectProps>> = new Map<String, Array<ProjectProps>>([["Java", [{name: "Course Review Website", link: "https://github.com/Pranie63/Course-Review-Website", imgPath: CourseReviewPic}, 
-    {name: "List Implementation", link: "https://github.com/Pranie63/Generic-Lists", imgPath: pic}, 
+const projectMappings: Map<String, Array<ProjectProps>> = new Map<String, Array<ProjectProps>>(
+    [["Java", 
+    [{name: "Course Review Website", link: "https://github.com/Pranie63/Course-Review-Website", imgPath: CourseReviewPic}, 
+    {name: "List Implementation", link: "https://github.com/Pranie63/Generic-Lists", imgPath: ListPic}, 
     {name: "Wordle", link: "https://github.com/Pranie63/Wordle", imgPath: WordlePic}]], 
-    ["Javascript", [{name: "test1", link: "", imgPath: ""}, 
-    {name: "1test1", link: "", imgPath: ""}]], 
-    ["Cybersecurity", [{name: "1", link: "", imgPath: ""}]], 
-    ["Robotics", [{name: "1", link: "", imgPath: ""}]]]);
+    ["Javascript", 
+    [{name: "eCommerce Bookstore", link: "https://github.com/Ananya-Ananda/eCommerceProject", imgPath: ""}]], 
+    ["Cybersecurity", [{name: "Blockchain", link: "https://github.com/Pranie63/PranavPenny", imgPath: ""}]], 
+    ["Robotics", [{name: "Racing Algorithms", link: "https://github.com/Pranie63/Racing-Algorithms/", imgPath: ""}]]]);
 const description: String = "This is a portfolio containing a collection of my most memorable pieces of work. They extend from personal projects to cumulative course work, representing my journey as a software engineer."
 
 function Portfolio() {
