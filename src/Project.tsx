3cef351eec28f5d81f92f80bf9fb95e092c54dfb
Pranie63 <Pranav Ramkumar>
2024-01-06 15:53:26 -0500

    Updated projects to link to repositories

--- src/Project.tsx
+++ src/Project.tsx
@@ -10,6 +10,7 @@ function Project(props: ProjectProps) {
     return (
         <div className='project' style={{background: "linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url(" + props.imgPath.toString() + ")"}}>
             {props.name}
+            <a href={props.link.toString()} target="_blank" rel="noopener noreferrer">Click to View Project</a>
         </div>
     )
 }
