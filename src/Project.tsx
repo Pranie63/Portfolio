e8bea9cebf8be11e5d85bc4b5733c9893fdbbfce
Pranie63 <Pranav Ramkumar>
2024-01-06 14:19:11 -0500

    Updated opacity code for project background images to avoid effect on text

--- src/Project.tsx
+++ src/Project.tsx
@@ -6,7 +6,7 @@ type ProjectProps = {
 
 function Project(props: ProjectProps) {
     return (
-        <div className='project' style={{backgroundImage: "url(https://t3.ftcdn.net/jpg/02/10/52/74/360_F_210527423_8Qdmmj7GlPEy0DknhPXtjigA54voMEDK.jpg)"}}>
+        <div className='project' style={{background: "linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url(" + props.imgPath.toString() + ")"}}>
             {props.name}
         </div>
     )
