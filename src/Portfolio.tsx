898d1fe611d6a5f37d48fef567e5ac599a3c478e
Pranie63 <Pranav Ramkumar>
2024-01-06 19:16:01 -0500

    Updated site with bookstore, blockchain, and racing algo pics

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -3,10 +3,12 @@ import './Portfolio.css';
 import Intro from './Intro';
 import Content from './Content';
 import type {ProjectProps} from './Project'
-import pic from './ProfilePic.jpg'
 import WordlePic from './WordlePic.png'
 import CourseReviewPic from './CourseReviewPic.png'
 import ListPic from './ListPic.png'
+import BookstorePic from './BookstorePic.png'
+import BlockchainPic from './BlockchainPic.png'
+import RacingAlgoPic from './RacingAlgoPic.jpg'
 
 // var test: Array<ProjectProps> = new Array<ProjectProps>({name: "test"}, {name: "test2"})
 const projectMappings: Map<String, Array<ProjectProps>> = new Map<String, Array<ProjectProps>>(
@@ -15,9 +17,9 @@ const projectMappings: Map<String, Array<ProjectProps>> = new Map<String, Array<
     {name: "List Implementation", link: "https://github.com/Pranie63/Generic-Lists", imgPath: ListPic}, 
     {name: "Wordle", link: "https://github.com/Pranie63/Wordle", imgPath: WordlePic}]], 
     ["Javascript", 
-    [{name: "eCommerce Bookstore", link: "https://github.com/Ananya-Ananda/eCommerceProject", imgPath: ""}]], 
-    ["Cybersecurity", [{name: "Blockchain", link: "https://github.com/Pranie63/PranavPenny", imgPath: ""}]], 
-    ["Robotics", [{name: "Racing Algorithms", link: "https://github.com/Pranie63/Racing-Algorithms/", imgPath: ""}]]]);
+    [{name: "eCommerce Bookstore", link: "https://github.com/Ananya-Ananda/eCommerceProject", imgPath: BookstorePic}]], 
+    ["Cybersecurity", [{name: "Blockchain", link: "https://github.com/Pranie63/PranavPenny", imgPath: BlockchainPic}]], 
+    ["Robotics", [{name: "Racing Algorithms", link: "https://github.com/Pranie63/Racing-Algorithms/", imgPath: RacingAlgoPic}]]]);
 const description: String = "This is a portfolio containing a collection of my most memorable pieces of work. They extend from personal projects to cumulative course work, representing my journey as a software engineer."
 
 function Portfolio() {
