6e10929249b496f40f4e9cfad4fa1d6ab182af8f
Pranie63 <Pranav Ramkumar>
2024-08-21 15:02:58 -0400

    Added Game Controller project to portfolio

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -9,17 +9,35 @@ import ListPic from './ListPic.png'
 import BookstorePic from './BookstorePic.png'
 import BlockchainPic from './BlockchainPic.png'
 import RacingAlgoPic from './RacingAlgoPic.jpg'
+import GameControllerPic from './GameControllerPic.png'
 
-// var test: Array<ProjectProps> = new Array<ProjectProps>({name: "test"}, {name: "test2"})
 const projectMappings: Map<String, Array<ProjectProps>> = new Map<String, Array<ProjectProps>>(
-    [["Java", 
-    [{name: "Course Review Website", link: "https://github.com/Pranie63/Course-Review-Website", imgPath: CourseReviewPic}, 
-    {name: "List Implementation", link: "https://github.com/Pranie63/Generic-Lists", imgPath: ListPic}, 
-    {name: "Wordle", link: "https://github.com/Pranie63/Wordle", imgPath: WordlePic}]], 
-    ["Javascript", 
-    [{name: "eCommerce Bookstore", link: "https://github.com/Ananya-Ananda/eCommerceProject", imgPath: BookstorePic}]], 
-    ["Cybersecurity", [{name: "Blockchain", link: "https://github.com/Pranie63/PranavPenny", imgPath: BlockchainPic}]], 
-    ["Robotics", [{name: "Racing Algorithms", link: "https://github.com/Pranie63/Racing-Algorithms/", imgPath: RacingAlgoPic}]]]);
+    [
+        ["Java", 
+            [
+                {name: "Course Review Website", link: "https://github.com/Pranie63/Course-Review-Website", imgPath: CourseReviewPic}, 
+                {name: "List Implementation", link: "https://github.com/Pranie63/Generic-Lists", imgPath: ListPic}, 
+                {name: "Wordle", link: "https://github.com/Pranie63/Wordle", imgPath: WordlePic}
+            ]
+        ], 
+        ["Javascript", 
+            [
+                {name: "eCommerce Bookstore", link: "https://github.com/Ananya-Ananda/eCommerceProject", imgPath: BookstorePic}
+            ]
+        ], 
+        ["Cybersecurity", 
+            [
+                {name: "Blockchain", link: "https://github.com/Pranie63/PranavPenny", imgPath: BlockchainPic}
+            ]
+        ], 
+        ["Robotics", 
+            [
+                {name: "Racing Algorithms", link: "https://github.com/Pranie63/Racing-Algorithms/", imgPath: RacingAlgoPic},
+                {name: "Game Controller", link: "https://github.com/Pranie63/Game-Controller", imgPath: GameControllerPic}
+            ]
+        ]
+    ]
+);
 const description: String = "This is a portfolio containing a collection of my most memorable pieces of work. They extend from personal projects to cumulative course work, representing my journey as a software engineer."
 
 function Portfolio() {
